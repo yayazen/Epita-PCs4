4ac3d6f86f246a7ef7cf09f02d5c49664ad0b959
Yanis Mammar <yanis.mammar@epita.fr>
2020-01-24 12:58:30 +0100

    update pw01 deadline 31/01

--- pw_01_tcp_client/main.c
+++ pw_01_tcp_client/main.c
@@ -4,9 +4,11 @@
 
 int main(int argc, char **argv) 
 {
-    if (argc != 2)
-        errx(EXIT_FAILURE, "(Argument error)");
-    
+    if (argc < 2)
+        errx(EXIT_FAILURE, "hostname is missing");
+    if (argc > 2)
+        errx(EXIT_FAILURE, "specify the hostname only");    
+
     print_page(argv[1]);
 
     return EXIT_SUCCESS;
--- pw_01_tcp_client/print_page.c
+++ pw_01_tcp_client/print_page.c
@@ -13,7 +13,7 @@ char *build_query(const char *host, size_t *len)
 {
     char *query;
     *len = asprintf(&query, "GET http://www.%s HTTP/1.0\r\n\r\n", host);
-
+    
     return query;
 }
 
@@ -28,7 +28,7 @@ void print_page(const char *host)
     struct addrinfo *res, *rp;
     int sfd, err;
 
-    memset(&hints, 0, sizeof(hints));
+    memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
 
@@ -48,15 +48,19 @@ void print_page(const char *host)
     }
 
     if (!rp)
-        errx(EXIT_FAILURE, "connect: Unsuccessful");
+        errx(EXIT_FAILURE, "connect: no address succeeded");
 
     freeaddrinfo(res);
 
 
     query = build_query(host, &len);
-
-    write(sfd, query, len);
-    while (read(sfd, buffer, BUFFER_SIZE)) {
+    if (write(sfd, query, len) != (ssize_t)len)
+       errx(EXIT_FAILURE, "query: partial/failed write to socket stream");  
+ 
+    while ((err = read(sfd, buffer, BUFFER_SIZE))) {
+        if (err == -1)
+            errx(EXIT_FAILURE, "read: partial/failed read from socket stream");
+        
         printf("%s", buffer);
     }
 
